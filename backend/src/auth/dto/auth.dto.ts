4a8768773d0d9716a7d7c2b9eb2c59ab923db1cf
dounia-elg <dounia.elg20@gmail.com>
2026-02-03 14:07:47 +0100

    feat: EV-1 add RegisterDto and LoginDto for user authentication

---
+++ backend/src/auth/dto/auth.dto.ts
@@ -0,0 +1,26 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(2)
+  firstName: string;
+
+  @IsString()
+  @MinLength(2)
+  lastName: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
