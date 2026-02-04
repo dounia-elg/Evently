e597cdf7cd0c21529883b52b4e228fa3062190b7
dounia-elg <dounia.elg20@gmail.com>
2026-02-04 14:15:00 +0100

    feat: EV-1 add TypeORM and PostgreSQL dependencies for database integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [],
