92fde71720c4a969df8921b8fead8b5a224ca7eb
dounia-elg <dounia.elg20@gmail.com>
2026-02-06 22:00:59 +0100

    feat: EV-60 add TicketsController to TicketsModule for ticket management

--- backend/src/tickets/tickets.module.ts
+++ backend/src/tickets/tickets.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ticket } from './entities/ticket.entity';
 import { TicketsService } from './tickets.service';
+import { TicketsController } from './tickets.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Ticket])],
+  controllers: [TicketsController],
   providers: [TicketsService],
   exports: [TicketsService],
 })
