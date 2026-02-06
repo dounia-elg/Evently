9d51a2a810a99a6bd5a8da03061e2055b7304f2c
dounia-elg <dounia.elg20@gmail.com>
2026-02-06 12:28:25 +0100

    feat: EV-45 add updateStatus endpoint to ReservationsController for updating reservation status

--- backend/src/reservations/reservations.controller.ts
+++ backend/src/reservations/reservations.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, Param, Patch } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { UserRole } from '../users/entities/user.entity';
+import { UpdateReservationStatusDto } from './dto/update-reservation-status.dto';
+
 
 @Controller('reservations')
 export class ReservationsController {
-  constructor(private reservationsService: ReservationsService) {}
+  constructor(private reservationsService: ReservationsService) { }
 
   @Post()
   @Roles(UserRole.PARTICIPANT)
   @UseGuards(JwtAuthGuard, RolesGuard)
   create(@Body() dto: CreateReservationDto, @Req() req: any) {
     return this.reservationsService.create(dto.eventId, req.user);
+  }
+
+  @Patch(':id/status')
+  @Roles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  updateStatus(@Param('id') id: string, @Body() dto: UpdateReservationStatusDto) {
+    return this.reservationsService.updateStatus(id, dto.status);
   }
 }
