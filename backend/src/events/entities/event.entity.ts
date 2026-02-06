7de5fdfd38ce23a1f212a3aaf5770af3aabc7188
dounia-elg <dounia.elg20@gmail.com>
2026-02-06 09:56:56 +0100

    feat: EV-25 add Event entity with properties and status enum

---
+++ backend/src/events/entities/event.entity.ts
@@ -0,0 +1,45 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+export enum EventStatus {
+  DRAFT = 'DRAFT',
+  PUBLISHED = 'PUBLISHED',
+  CANCELED = 'CANCELED',
+}
+
+@Entity('events')
+export class Event {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  description: string;
+
+  @Column()
+  dateTime: Date;
+
+  @Column()
+  location: string;
+
+  @Column()
+  maxCapacity: number;
+
+  @Column({
+    type: 'enum',
+    enum: EventStatus,
+    default: EventStatus.DRAFT,
+  })
+  status: EventStatus;
+
+  @ManyToOne(() => User)
+  admin: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
